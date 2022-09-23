732d30e276d54697fe94839e77c6291cee8f83a1
Siarhei Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-09-23 16:47:26 +0300

    del home

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   return (
     <NavbarCard>
       {/* <h1>Phonebook</h1> */}
-      <HeaderLink to="/home" >Phonebook</HeaderLink>
+      <HeaderLink to="/" >Phonebook</HeaderLink>
       {isLogin && <NavbarMenu />}
       {isLogin ? <NavbarUser /> : <NavbarAuth />}
     </NavbarCard>
